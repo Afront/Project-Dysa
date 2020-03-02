aaa20df0b7e3f4ca02bfc0cbeceb9ccc0373306d
Afront <3943720+Afront@users.noreply.github.com>
2020-03-03 00:16:34 +0900

    Update sort.rb

--- Arrays/Algorithms/Sorts/sort.rb
+++ Arrays/Algorithms/Sorts/sort.rb
@@ -1,21 +1,18 @@
 # frozen_string_literal: true
 
 def not_sorted(words)
-  (words.length - 1).times do |i|
-    return true if words[i] > words[i + 1]
-  end
-  false
+  words.each_cons(2).all? { |a, b| a > b }
 end
 
 def sort(words)
   while not_sorted(words)
-    (words.length - 1).times do |i|
+    words.length.pred.times do |i|
       next unless words[i] > words[i + 1]
-    
+
       words[i], words[i + 1] = words[i + 1], words[i]
     end
   end
   words
 end
 
-p sort %w[a z b aaa cafe awefw]
+sort %w[a z b aaa cafe awefw]
