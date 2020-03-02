a963bf4f5be369182b63f070afb950317c51ad10
Afront <3943720+Afront@users.noreply.github.com>
2020-03-02 23:55:35 +0900

    Lint sort.rb code

--- Arrays/Algorithms/Sorts/sort.rb
+++ Arrays/Algorithms/Sorts/sort.rb
@@ -1,34 +1,23 @@
+# frozen_string_literal: true
+
 def not_sorted(words)
-	(words.length-1).times do |i|
-		return true if words[i] > words[i+1]
-	end
-	return false
+  (words.length - 1).times do |i|
+    return true if words[i] > words[i + 1]
+  end
+  false
 end
 
-def sort words
-	while not_sorted(words)
-		(words.length-1).times do |i|
-			if words[i] > words[i+1]
-				tmp = words[i]
-				words[i] = words[i+1]
-				words[i+1] = tmp
-			end
-		end
-	end
-	return words
+def sort(words)
+  while not_sorted(words)
+    (words.length - 1).times do |i|
+      next unless words[i] > words[i + 1]
+
+      tmp = words[i]
+      words[i] = words[i + 1]
+      words[i + 1] = tmp
+    end
+  end
+  words
 end
 
-def sort_c words
-	while not_sorted(words){
-		(words.length-1).times { |i|
-			if (words[i] > words[i+1]){
-				tmp = words[i];
-				words[i] = words[i+1];
-				words[i+1] = tmp;
-			}
-		}
-	}
-	return words;
-end
-
-p sort_c ["a", "z", "b", 'aaa', 'cafe', 'awefw']
+p sort %w[a z b aaa cafe awefw]
