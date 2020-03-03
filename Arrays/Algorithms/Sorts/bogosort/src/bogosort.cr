09bcece5d7d9d9c49805022c9a4094babdebfa62
Afront <3943720+Afront@users.noreply.github.com>
2020-03-03 23:55:01 +0900

    Lint Bogosort code using Ameba (Crystal)

--- Arrays/Algorithms/Sorts/bogosort/src/bogosort.cr
+++ Arrays/Algorithms/Sorts/bogosort/src/bogosort.cr
@@ -1,13 +1,19 @@
-# TODO: Write documentation for `Bogosort`
+# Contains the code for the bogosort
 module Bogosort
   VERSION = "0.1.0"
 
-  def self.sorted(array)
+  # Checks if the array is sorted
+  private def self.sorted(array)
     array.each_cons(2).all? { |i| i[0] <= i[1] }
   end
 
+  # Sorts the array using bogosort
+  #
+  # ```
+  # Bogosort.sort([3, 2, 1]) # => [1, 2, 3]
+  # ```
   def self.sort(array : Array)
-    until sorted(array)
+    until sorted array
       array.shuffle!
     end
     array
