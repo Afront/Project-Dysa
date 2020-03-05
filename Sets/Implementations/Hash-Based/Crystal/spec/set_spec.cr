a8917ac97c7660058dc0f6bc1985ccd26b089b41
Afront <3943720+Afront@users.noreply.github.com>
2020-03-05 23:29:10 +0900

    Add #inspect and #to_s to the hash-based set (Crystal)

--- Sets/Implementations/Hash-Based/Crystal/spec/set_spec.cr
+++ Sets/Implementations/Hash-Based/Crystal/spec/set_spec.cr
@@ -13,4 +13,16 @@ describe NewSet do
       p a
     end
   end
+
+  describe "#to_s" do
+    it "should turn a set into a string based on its values" do
+      NewSet::BaseSet.new([1, 2, 3]).to_s.should eq("{1, 2, 3}")
+    end
+  end
+
+  describe "#inspect" do
+    it "should 'return' the set as a string with brackets" do
+      NewSet::BaseSet(Int32).new.inspect.should eq("{}")
+    end
+  end
 end
--- Sets/Implementations/Hash-Based/Crystal/src/set.cr
+++ Sets/Implementations/Hash-Based/Crystal/src/set.cr
@@ -3,6 +3,10 @@ module NewSet
   VERSION = "0.1.0"
 
   class BaseSet(T)
+    include Enumerable(T) # for `join`
+
+    # include Comparable(BaseSet)
+
     def initialize
       @hash = Hash(T, Nil).new
     end
@@ -13,6 +17,29 @@ module NewSet
       enumerable.each do |key|
         @hash[key] = nil
       end
+    end
+
+    # Converts the base set into a string
+    #
+    # ```
+    # BaseSet.new([1, 2]).to_s # => "[1,2]"
+    # ```
+    def to_s(io : IO) : Nil
+      is_executed = exec_recursive(:to_s) do
+        io << '{'
+        join ", ", io, &.inspect(io)
+        io << '}'
+      end
+      io << "{...}" unless is_executed # probably not needed
+    end
+
+    # "Appends the String representation" of the base set
+    #
+    # ```
+    # BaseSet.new([1, 2]).to_s # => "[1,2]"
+    # ```
+    def inspect(io : IO) : Nil
+      to_s io
     end
 
     def each
