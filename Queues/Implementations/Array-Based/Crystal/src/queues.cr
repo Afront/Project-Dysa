096f2c371363fb3b7b5ff32500052a596e6fcee5
Afront <3943720+Afront@users.noreply.github.com>
2020-03-17 22:47:53 +0900

    Fix documentation in the Crystal implementation of Queue

--- Queues/Implementations/Array-Based/Crystal/src/queues.cr
+++ Queues/Implementations/Array-Based/Crystal/src/queues.cr
@@ -8,61 +8,61 @@ module Queues
     # Initializes a queue without any arguments (aside from the type of course)
     #
     # ```
-    # Stack(Int32).new.to_a # => []
+    # Queue(Int32).new.to_a # => []
     # ```
     def initialize
-      @stack = Stack.new
+      @stack = Stack(T).new
     end
 
     # Initializes a queue with an *initial_capacity*
     #
     # ```
-    # Stack(Int32).new(1).to_a # => []
+    # Queue(Int32).new(1).to_a # => []
     # ```
     def initialize(initial_capacity : Int)
-      @stack = Stack.new(initial_capacity)
+      @stack = Stack(T).new(initial_capacity)
     end
 
     # Initializes a queue of the *size* given the that contains only the given *value*
     #
     # ```
-    # Stack.new(2, 10).to_a # => [10,10]
+    # Queue.new(2, 10).to_a # => [10,10]
     # ```
     def initialize(size : Int, value : T)
-      @stack = Stack.new(size, value)
+      @stack = Stack(T).new(size, value)
     end
 
     # Initializes a queue with an *array*
     #
     # ```
-    # Stack.new([1, 2]).to_a # => [1,2]
+    # Queue.new([1, 2]).to_a # => [1,2]
     # ```
     def initialize(array : Array(T))
-      @stack = Stack.new(array)
+      @stack = Stack(T).new(array)
     end
 
     # Initializes a queue with a given *size* and a *block* that modifies the queue
     #
     # ```
-    # Stack.new(2) { |i| i * 2 }.to_a # => [0,2]
+    # Queue.new(2) { |i| i * 2 }.to_a # => [0,2]
     # ```
     def initialize(size : Int, &block : Int32 -> T)
-      @stack = Stack.new(size, &block)
+      @stack = Stack(T).new(size, &block)
     end
 
     # Initializes a queue with an *array* and a *block* that modifies the queue
     #
     # ```
-    # Stack.new([1, 2]) { |i| i + 1 }.to_a # => [2,3]
+    # Queue.new([1, 2]) { |i| i + 1 }.to_a # => [2,3]
     # ```
     def initialize(array : Array(T), &block : Int32 -> T)
-      @stack = array.map(&block)
+      @stack = Stack.new(array.map(&block))
     end
 
     # Converts the queue into an array
     #
     # ```
-    # Stack.new([1, 2]).to_a # => [1,2]
+    # Queue.new([1, 2]).to_a # => [1,2]
     # ```
     def to_a
       @stack.to_a
@@ -71,7 +71,7 @@ module Queues
     # Converts the queue into a string
     #
     # ```
-    # Stack.new([1, 2]).to_s # => "[1,2]"
+    # Queue.new([1, 2]).to_s # => "[1,2]"
     # ```
     def to_s(io : IO) : Nil
       @stack.to_s io
@@ -80,7 +80,7 @@ module Queues
     # "Appends the String representation" of the queue
     #
     # ```
-    # Stack.new([1, 2]).to_s # => "[1,2]"
+    # Queue.new([1, 2]).to_s # => "[1,2]"
     # ```
     def inspect(io : IO) : Nil
       to_s io
@@ -89,7 +89,7 @@ module Queues
     # Checks if the queue is empty or not
     #
     # ```
-    # Stack(Int32).new.empty? # => true
+    # Queue(Int32).new.empty? # => true
     # ```
     def empty?
       @stack.empty?
@@ -98,16 +98,16 @@ module Queues
     # Looks at the last value (or the 'top') of the queue
     #
     # ```
-    # Stack.new([1, 2]).peek # => 2
+    # Queue.new([1, 2]).peek # => 2
     # ```
     def peek
-      @stack[-1]
+      @stack.peek
     end
 
     # Pushes a value into a queue
     #
     # ```
-    # Stack.new([1, 2]).push(2).to_a # => [1,2,2]
+    # Queue.new([1, 2]).unshift(2).to_a # => [2, 1, 2]
     # ```
     def unshift(value : T)
       @stack.push value
@@ -116,16 +116,16 @@ module Queues
     # Pops a value from a queue
     #
     # ```
-    # Stack.new([1, 2]).pop # => 2
+    # Queue.new([1, 2]).shift # => 1
     # ```
     def shift
-      @array.pop
+      @stack.pop
     end
 
     # Compares the queue with another queue based on their values
     #
     # ```
-    # Stack.new([1, 2]) == Stack.new([1, 2]) # => true
+    # Queue.new([1, 2]) == Stack(T).new([1, 2]) # => true
     # ```
     def ==(other : Queue)
       to_a == other.to_a
