ec015d944a8d8e4ec3c41a70bd6746e80874585f
Afront <3943720+Afront@users.noreply.github.com>
2020-03-19 23:54:32 +0900

    Add #initialize to Node for Binary Tree (Crystal)

--- Trees/Implementations/Binary/Crystal/spec/Binary_spec.cr
+++ Trees/Implementations/Binary/Crystal/spec/Binary_spec.cr
@@ -1,9 +1,27 @@
 require "./spec_helper"
+include Binary
 
 describe Binary do
   # TODO: Write tests
+  describe Node do
+    describe "#initialize" do
+      it "can be created using any type of data" do
+        Node.new(1)
+        Node.new(1.0)
+        Node.new('a')
+        Node.new("a")
+        Node.new(nil)
+      end
+    end
 
-  it "works" do
-    false.should eq(true)
+    it "can get left value" do
+      node = Node.new(1)
+      node.left.should be_nil
+    end
+
+    it "can get right value" do
+      node = Node.new(1)
+      node.left.should be_nil
+    end
   end
 end
--- Trees/Implementations/Binary/Crystal/src/Binary.cr
+++ Trees/Implementations/Binary/Crystal/src/Binary.cr
@@ -2,5 +2,13 @@
 module Binary
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
+  class Node(T)
+    property data
+    getter left : Node(T)?, right : Node(T)?
+
+    def initialize(@data : T)
+      left = nil
+      right = nil
+    end
+  end
 end
