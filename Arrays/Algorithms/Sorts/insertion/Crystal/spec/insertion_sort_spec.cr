cfd07bdb1e2f34150193aaf3075008fe6abe29a2
Afront <3943720+Afront@users.noreply.github.com>
2020-03-29 23:58:03 +0900

    Add partial code for insertion sort

--- Arrays/Algorithms/Sorts/insertion/Crystal/spec/insertion_sort_spec.cr
+++ Arrays/Algorithms/Sorts/insertion/Crystal/spec/insertion_sort_spec.cr
@@ -1,9 +1,19 @@
 require "./spec_helper"
 
 describe InsertionSort do
-  # TODO: Write tests
+  it "works for floats" do
+    InsertionSort.sort([2.5, 1]).should eq([1, 2.5])
+    InsertionSort.sort([1.0]).should eq([1.0])
+    InsertionSort.sort([51.2, 2.1, 3.4, Math::PI]).should eq([2.1, Math::PI, 3.4, 51.2])
+  end
 
-  it "works" do
-    false.should eq(true)
+  it "works for ints" do
+    InsertionSort.sort([2, 1]).should eq([1, 2])
+    InsertionSort.sort([1]).should eq([1])
+    InsertionSort.sort([5, 2, 3]).should eq([2, 3, 5])
+  end
+
+  it "works for strings" do
+    InsertionSort.sort(%w[they are you]).should eq(%w[are they you])
   end
 end
--- Arrays/Algorithms/Sorts/insertion/Crystal/src/insertion_sort.cr
+++ Arrays/Algorithms/Sorts/insertion/Crystal/src/insertion_sort.cr
@@ -1,6 +1,15 @@
 # TODO: Write documentation for `InsertionSort`
-module InsertionSort
+module InsertionSort(T)
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
+  def self.sort(array : Array(T))
+    # return Bogosort.sort(array) if array.size <= 3
+    (0..array.size.pred).each do |i|
+      j = i + 1
+      while j > 0 && array[j.pred] > array[j]
+        array[j], array[j - 1] = array[j - 1], array[j]
+      end
+      i += 1
+    end
+  end
 end
