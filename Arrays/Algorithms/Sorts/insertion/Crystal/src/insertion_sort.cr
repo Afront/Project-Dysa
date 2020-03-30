7514593d2183215109c3de6a98e03b50f891831f
Afront <3943720+Afront@users.noreply.github.com>
2020-03-31 00:33:16 +0900

    Make code for insertion sort work

--- Arrays/Algorithms/Sorts/insertion/Crystal/src/insertion_sort.cr
+++ Arrays/Algorithms/Sorts/insertion/Crystal/src/insertion_sort.cr
@@ -3,14 +3,13 @@ module InsertionSort(T)
   VERSION = "0.1.0"
 
   def self.sort(array : Array(T))
-    # return Bogosort.sort(array) if array.size <= 3
-    1.upto(array.size) do |i|
-      j = i.pred
-      while j >= 0 && array[j] > array[j.succ]
-        array[j.succ] = array[j]
+    array.size.times do |i|
+      j = i
+      while j > 0 && array[j.pred] > array[j]
+        array[j], array[j.pred] = array[j.pred], array[j]
         j -= 1
       end
-      array[j.succ] = array[j]
     end
+    array
   end
 end
